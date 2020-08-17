833e76fc09323743a6bd83462a86e04a51a8e783
John Tucker <john@larkintuckerllc.com>
2020-08-17 15:16:31 -0400

    login

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express');
 const dotenv = require('dotenv');
+const express = require('express');
+const googleapis = require('googleapis');
+
+const { google } = googleapis;
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
+const auth = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+const loginUri = auth.generateAuthUrl({
+  access_type: 'offline',
+  prompt: 'consent',
+  scope: process.env.SCOPE,
+});
+
 const app = express();
-const port = 3000;
+
+app.get('/login', (req, res) => res.redirect(loginUri));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
-
