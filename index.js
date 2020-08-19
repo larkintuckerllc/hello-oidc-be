fd63ca61d0e90ea6ee6e15f628c89899e9f5441b
John Tucker <john@larkintuckerllc.com>
2020-08-18 22:18:37 -0400

    flow

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
 const googleapis = require('googleapis');
@@ -15,20 +16,39 @@ const auth = new google.auth.OAuth2(
 );
 
 const app = express();
+app.use(cors())
 
-app.get('/login', (req, res) => {
+app.get('/login-screen', (req, res) => {
   const { nonce, state } = req.query;
   if (state === undefined || nonce === undefined) {
-    res.status(400).send('missing state URL parameters state and nonce');
+    res.status(400).send('missing state and nonce URL parameters');
     return;
   }
   const loginUri = auth.generateAuthUrl({
     access_type: 'offline',
+    prompt: 'consent',
     nonce,
     scope: 'https://www.googleapis.com/auth/userinfo.email',
     state,
   });
   res.redirect(loginUri);
+});
+
+app.get('/get-tokens', async (req, res) => {
+  const { code } = req.query;
+  if (code === undefined) {
+    res.status(400).send('missing code URL parameters');
+    return;
+  }
+  try {
+    const { tokens: { id_token, refresh_token } } = await auth.getToken(code);
+    res.send({
+      id_token,
+      refresh_token,
+    });
+  } catch (err) {
+    res.status(401).send('unauthorized');
+  }
 });
 
 app.get('/', (req, res) => {
